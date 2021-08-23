502ad7640564863237f9e51740aa5639657563b0
JosephMagiya <anyonajoseph@gmail.com>
2021-08-23 13:18:08 +0300

    first commit

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn TalentSearchProject.wsgi --log-file -
---
+++ static/js/main.js
@@ -0,0 +1,49 @@
+
+// Get search form and page links
+let searchForm = document.getElementById('searchForm')
+let pageLinks = document.getElementsByClassName('page-link')
+
+// Ensure search Form Exists
+if (searchForm) {
+    for (let i = 0; pageLinks.length > i; i++) {
+        pageLinks[i].addEventListener('click', function (e) {
+            e.preventDefault()
+            // Get the data attribute
+            let page = this.dataset.page
+
+            // Add hidden search input to form
+            searchForm.innerHTML += `<input value=${page} name="page" hidden/>`
+
+            // Submit Form
+            searchForm.submit()
+        })
+    }
+}
+
+
+
+let tags = document.getElementsByClassName('project-tag')
+
+for (let i = 0; tags.length > i; i++) {
+    tags[i].addEventListener('click', (e) => {
+        let tagId = e.target.dataset.tag
+        let projectId = e.target.dataset.project
+
+        // console.log('TAG ID:', tagId)
+        // console.log('PROJECT ID:', projectId)
+
+        // fetch('http://127.0.0.1:8000/api/remove-tag/', {
+        fetch('http://talentsearch1.herokuapp.com/api/remove-tag/', {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ 'project': projectId, 'tag': tagId })
+        })
+            .then(response => response.json())
+            .then(data => {
+                e.target.remove()
+            })
+
+    })
+}
